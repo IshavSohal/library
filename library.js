c353cd8461416162a6e04eb6879849bbfe3df50c
Ishav Sohal <ishav.sohal@mail.utoronto.ca>
2025-05-04 14:32:35 -0400

    refactor to use classes

--- library.js
+++ library.js
@@ -10,17 +10,18 @@ const bookTitleInput = document.querySelector("#book-title");
 const bookAuthorInput = document.querySelector("#book-author");
 const numPagesInput = document.querySelector("#num-pages");
 
-function Book(title, author, pages, read = false) {
-    this.id = crypto.randomUUID();
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 
-Book.prototype.toggleRead = function () {
-    this.read = !this.read;
-};
+    toggleRead() {
+        this.read = !this.read;
+    }
+}
 
 function addBookToLibrary(title, author, pages, read) {
     const newBook = new Book(title, author, pages, read);
